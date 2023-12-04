bce0d18c54026c0288db45512057b739689128ad
Italo <italo.rivadeneyra.r@uni.pe>
2023-12-03 23:50:19 -0500

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,16 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'AppCliente';
+
+  nombre: any = 'Italo';
+  apellido: any = 'Perez';
+
+  mostrarNombre = true;
+
+  alumnos: any = ['Italo', 'Jose', 'Pepe', 'Lucho'];
+  nombreCurso = 'Numeros';
 }
