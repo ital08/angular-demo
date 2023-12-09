297a2eb187408387ee33b594ec3efe5e9a8c8f89
Italo <italo.rivadeneyra.r@uni.pe>
2023-12-09 12:43:20 -0500

    CLASE 09/12

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # AppCliente
 
 https://angular.io/
+
 Jr. Fullstack
-*Crear un modulo de angular
-*Crear un componente
+
+*Crear una aplicacion de angular /// ng new nombre_aplicacion
+*Enter como funcionan las rutas en angular // app.routes.ts
+*Crear un componente /// ng g c ruta/nombre_componente
+*Crear un componente que sea un formulario // USAREMOS BOOSTRAP https://material.angular.io/components/categories
+*Crear un componente que sea un formulario reactivo
 *Crear un servicio
-*Crear una
-aplicacion .NET *Crear un servicio de .NET
+*Crear una aplicacion .NET *Crear un servicio de .NET
 *Vamos a integrar nuestro servicio de
 angular con el serivico de .NET usando Dapper
-\*Vamos a utilizar una base de datos para que la
+*Vamos a utilizar una base de datos para que la
 información viaje del servicio de .NET a nuestra vista en angular
 
 Ssr.
 *Crear un modulo de angular
-*Crear un componente, directivas, pipes, librerias de estilos
+*Crear un componente, directivas, pipes, librerias de estilos, input output
 *Crear un servicio, http
 *Crear una
 aplicacion .NET *Crear un servicio de .NET usando un patron de diseño
--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSliderModule,
+    MatButtonModule,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  formGroup!: FormGroup;
 
+  ngOnInit(): void {
+    this.inicializarFormulario();
+  }
+
+  inicializarFormulario() {
+    this.formGroup = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10),
+      ]),
+      remuneracion: new FormControl('', []),
+      genero: new FormControl('', []),
+    });
+  }
+
+  email: string = '';
+  nombre: string = '';
+  remuneracion: string = '';
+  genero: string = '';
+
+  public imprimirDatosFormulario() {
+    if (this.email && this.nombre && this.remuneracion && this.genero) {
+      alert('Formulario correctamente llenado');
+    } else {
+      alert('Hay campos que no han sido llenados. Porfavor completar');
+    }
+  }
 }
